e3e3e86cecc7bcd482fabe52c70a0d1c0585e70a
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-07-02 13:04:27 -0700

    added js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+let upperLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let lowerLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let numberChar  = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let specialChar = ["!", "@", "$", "%", "&", "-"]
+
+let charCount
+let useUpper   
+let useLower    
+let useNumber   
+let useSpecial 
+let newPassword = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -26,37 +38,75 @@ function generatePassword(){
         return
       }
     } while(charCount < 8 || charCount > 128)
-  
-    //confirms the character count in the console
-    console.log(charCount)
 
     //converts string type to number type
     charCount = Number(charCount)
 
-    //determines if the user wants to use uppercase letters
-    useUpper = confirm("Would you like to use Uppercase letters?")
-    if(useUpper){
+    do{
+      //determines if the user wants to use uppercase, lowercase, number or special characters
+      useUpper   = confirm("Would you like to use Uppercase letters?");
+      useLower   = confirm("Would you like to use Lowercase letters?");
+      useNumber  = confirm("Would you like to use Numbers?")
+      useSpecial = confirm("Would you like to use Special Characters?")
+
+      if(useUpper == false && useLower == false && useNumber == false && useSpecial == false){
+        alert("You must confirm at least one of these conditions")
+      }
+    } while(useUpper == false && useLower == false && useNumber == false && useSpecial == false)
+
+
+    // adds all the characters of the password together into one string
+    function concatPassword(){
+
+      function useUpperChar(){
+        if(useUpper == true){
+          let newUpper = upperLetter[Math.floor(Math.random() * upperLetter.length)]
+          newPassword.push(newUpper)
+        }
+        else{
+          charCount
+        }
+      }
+  
+      function useLowerChar(){
+        if(useLower == true){
+          let newLower = lowerLetter[Math.floor(Math.random() * lowerLetter.length)]
+          newPassword.push(newLower)
+        }else{
+          charCount
+        }
+      }
+  
+      function useNumberChar(){
+        if(useNumber == true){
+          let newNumber = numberChar[Math.floor(Math.random() * numberChar.length)]
+          newPassword.push(newNumber)
+        }else{
+          charCount
+        }
+      }
+  
+      function useSpecialChar(){
+        if(useSpecial == true){
+          let newSpecial = specialChar[Math.floor(Math.random() * specialChar.length)]
+          newPassword.push(newSpecial)
+        }else{
+          charCount
+        }
+      }
+
       
-    } 
+      do{
+
+        let randomChar = [useUpperChar(), useLowerChar(), ,useNumberChar(), useSpecialChar()];
+        
+        newPassword.push(randomChar[Math.floor(Math.random() * randomChar.length)])
+
+      }while(newPassword.length < charCount)
     
-
-
+    return newPassword.toString().replaceAll(",", "")
+  }
 
     //needs to return a value for the writePassword function
-    return charCount + charCount
-
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
+    return String(concatPassword())
 }
