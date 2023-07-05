defba00ff3a44dee8e3829e4e6bfa3ff66a87fa3
Jacob Kovacs <jwkovacs.19@gmail.com>
2023-07-05 16:33:58 -0700

    added more special character selection

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 let upperLetter = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let lowerLetter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 let numberChar  = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-let specialChar = ["!", "@", "$", "%", "&", "-"]
+let specialChar = ["!", "@", "$", "%", "&", "-", " ", "{", "}", "(", ")", "[", "]", "*", "#", "+", "/", ";", ":", ",", "."]
 
 let charCount
 let useUpper   
